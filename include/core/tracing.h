4fe7e265306033a6ad0d16d8d5b2853ad810a8da
Guy John <guy@rumblesan.com>
2015-11-07 16:19:20 +0000

    Adding tracing functions

---
+++ include/core/tracing.h
@@ -0,0 +1,27 @@
+#ifndef TRACING_H
+#define TRACING_H
+
+#include "core/colours.h"
+#include "core/scene.h"
+#include "core/shapes.h"
+#include "core/ray.h"
+
+typedef struct intersection {
+
+    Shape object;
+
+    double distance;
+
+} Intersection;
+
+void rays_calc(Scene scene);
+
+Colour trace(Ray ray, Scene scene, int depth);
+
+Intersection intersectedObject(Ray ray, Scene scene);
+
+Colour surface(Ray ray, Scene scene, Shape object, Vector3D intersection, int depth);
+
+bool light_is_visible(Vector3D intersection, Scene scene, Light light);
+
+#endif
---
+++ src/core/tracing.c
@@ -0,0 +1,142 @@
+#include <stdlib.h>
+#include <math.h>
+
+#include "core/tracing.h"
+
+#include "core/colours.h"
+#include "core/scene.h"
+#include "core/shapes.h"
+#include "core/ray.h"
+#include "core/vector.h"
+#include "core/camera.h"
+#include "core/config.h"
+
+
+void rays_calc(Scene scene) {
+
+    int x, y;
+    Config config = scene->config;
+    Camera camera = scene->camera;
+
+    double fovRadians   = tan((M_PI / 180) * (camera->fov/2));
+    double heightWidthRatio = config->height/config->width;
+    double halfWidth    = tan(fovRadians);
+    double halfHeight   = heightWidthRatio * halfWidth;
+    double cameraWidth  = halfWidth * 2;
+    double cameraHeight = halfHeight * 2;
+    double pixelWidth   = cameraWidth / (config->width - 1);
+    double pixelHeight  = cameraHeight / (config->height - 1);
+
+    Vector3D eyeVector = vector3d_unit(
+        vector3d_subtract(
+             camera->looking_at, camera->position
+        )
+    );
+    Vector3D vpRight = vector3d_unit(
+        vector3d_cross(
+             eyeVector, vector3d(0, 1.0, 0)
+        )
+    );
+    Vector3D vpUp = vector3d_unit(
+        vector3d_cross(
+             vpRight, eyeVector
+        )
+    );
+
+    Vector3D xcomp, ycomp;
+
+    int depth = 0;
+    Ray pxRay;
+    Vector3D rayVector;
+    Colour pxColour;
+    for (x = 0; x < config->width; x += 1) {
+        for (y = 0; y < config->height; y += 1) {
+            xcomp = vector3d_scale((x * pixelWidth) - halfWidth, vpRight);
+            ycomp = vector3d_scale((x * pixelHeight) - halfHeight, vpUp);
+            rayVector = vector3d_unit(
+                vector3d_add(
+                    vector3d_add(
+                        eyeVector, xcomp
+                    ),
+                    ycomp
+                )
+            );
+            pxRay = ray(camera->position, rayVector);
+            pxColour = trace(pxRay, scene, depth);
+            canvas_set(scene->canvas, x, y, pxColour);
+        }
+    }
+
+}
+
+Colour trace(Ray ray, Scene scene, int depth) {
+
+    Intersection distObject = intersectedObject(ray, scene);
+
+    if (distObject.object == NULL) {
+        return scene->config->background;
+    }
+
+    Vector3D intersectPoint = vector3d_add(
+        ray.point,
+        vector3d_scale(distObject.distance, ray.vector)
+    );
+
+    return surface(ray, scene, distObject.object, intersectPoint, depth);
+}
+
+Intersection intersectedObject(Ray ray, Scene scene) {
+    Intersection distObject;
+    distObject.object = NULL;
+    distObject.distance = 0;
+
+    ShapeList shapes = scene->shapes;
+
+    double d;
+    while (!shape_list_empty(shapes)) {
+        d = shape_intersect(shapes->head, ray);
+        if (d > distObject.distance) {
+            distObject.object = shapes->head;
+            distObject.distance = d;
+        }
+        shapes = shape_list_tail(shapes);
+    }
+
+    return distObject;
+}
+
+Colour surface(Ray ray, Scene scene, Shape object, Vector3D intersection, int depth) {
+    Colour sc = colour(0, 200, 0, 255);
+
+    Vector3D normal = shape_normal(object, intersection);
+    LightList lights = scene->lights;
+    Light light;
+    double contribution;
+    double lambertAmount = 0;
+
+    while(!light_list_empty(lights)) {
+        light = lights->head;
+        if (light_is_visible(intersection, scene, light)) {
+            contribution = vector3d_dot(
+                vector3d_unit(vector3d_subtract(light.centre, intersection)),
+                normal
+            );
+            if (contribution > 0) {
+                lambertAmount += contribution;
+            }
+        }
+    }
+
+    return colour_mult(sc, fmin(1, lambertAmount));
+}
+
+bool light_is_visible(Vector3D intersection, Scene scene, Light light) {
+    Intersection distObject = intersectedObject(
+        ray(intersection, vector3d_unit(
+            vector3d_subtract(intersection, light.centre)
+        )),
+        scene
+    );
+    return (distObject.distance > -0.005);
+}
+
