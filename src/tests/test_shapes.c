efdea337deeae4e14d237123b27b8c357d41d704
Guy John <guy@rumblesan.com>
2015-11-08 12:17:07 +0000

    Adding intersect test to shapes

--- src/tests/test_shapes.c
+++ src/tests/test_shapes.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "tests/min_unit.h"
 
 #include "core/shapes.h"
@@ -12,8 +14,25 @@ static char * test_creation() {
     return 0;
 }
 
+static char * test_intersect() {
+
+    Ray r = ray(
+        vector3d(0.0, 0.0, 0.0), vector3d(1.0, 0.0, 0.0)
+    );
+    Shape shape = sphere_create(2.0, 0.0, 0.0, 1.0);
+
+    double distance = shape_intersect(shape, r);
+
+    mu_assert("Error: Intersection not happening", distance > 0);
+    mu_assert("Error: Intersection distance incorrect", distance == 1.0);
+
+    return 0;
+}
+
 char * test_shapes() {
+    printf("Running shape tests\n");
     mu_run_test(test_creation);
+    mu_run_test(test_intersect);
     return 0;
 }
 
