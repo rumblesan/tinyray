f1c80e5a285969bbd59b38c44dd98bc1e57c225a
Guy John <guy@rumblesan.com>
2016-03-17 11:57:54 +0000

    Implementing basic reference counting for datavalues

--- traylang/include/datavalue.h
+++ traylang/include/datavalue.h
@@ -9,8 +9,16 @@ typedef struct DataValue {
 
     void *value;
 
+    int ref_count;
+
 } DataValue;
 
 DataValue *datavalue_create(DataType type, void *value);
+
+DataValue *datavalue_incr_ref(DataValue *value);
+
+void *datavalue_decr_ref(DataValue *value);
+
+void datavalue_destroy(DataValue *value);
 
 #endif
--- traylang/src/datavalue.c
+++ traylang/src/datavalue.c
@@ -8,8 +8,36 @@ DataValue *datavalue_create(DataType type, void *value) {
     check_mem(data_value);
     data_value->type = type;
     data_value->value = value;
+    data_value->ref_count = 1;
     return data_value;
 error:
     return NULL;
+}
+
+DataValue *datavalue_incr_ref(DataValue *value) {
+    check(value, "DataValue is null");
+    value->ref_count += 1;
+    return value;
+error:
+    return NULL;
+}
+
+void *datavalue_decr_ref(DataValue *value) {
+    check(value, "DataValue is null");
+    check(value->ref_count >= 1, "DataValue ref count can't be less than 0");
+    value->ref_count -= 1;
+    if (value->ref_count == 0) {
+        free(value);
+    }
+    return NULL;
+error:
+    if (value) {
+        free(value);
+    }
+    return NULL;
+}
+
+void datavalue_destroy(DataValue *value) {
+    free(value);
 }
 
