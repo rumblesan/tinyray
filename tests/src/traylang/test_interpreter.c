c7a1c2ae632cbfb1e1c7d75fb09611fc384c6b8a
Guy John <guy@rumblesan.com>
2016-03-18 09:58:50 +0000

    Rejig tests a bit

---
+++ tests/src/traylang/test_interpreter.c
@@ -0,0 +1,28 @@
+#include "minunit.h"
+
+#include "interpreter.h"
+
+static Interpreter *interpreter = NULL;
+
+
+static char * test_create() {
+    interpreter = interpreter_create();
+    mu_assert(interpreter != NULL, "failed to create interpreter");
+    return 0;
+}
+
+static char *test_destroy() {
+    interpreter_destroy(interpreter);
+    return 0;
+}
+
+char *all_tests() {
+    mu_suite_start();
+
+    mu_run_test(test_create);
+    mu_run_test(test_destroy);
+
+    return NULL;
+}
+
+RUN_TESTS(all_tests);
